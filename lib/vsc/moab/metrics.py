ceec9f8a4478645b02db87f41e33de8fa6c9f43f
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-06-19 08:50:00 +0200

    Add time series

--- lib/vsc/moab/metrics.py
+++ lib/vsc/moab/metrics.py
@@ -5,6 +5,46 @@ evolution of the queues.
 
 import pandas as pd
 
+def get_time_stamp(df):
+    '''
+    Return the time stemp of a showq data frame.
+      df: pandas data frame representing showq output
+      returns a pandas Datetime object
+    '''
+    return df.time_stamp.drop_duplicates()[0]
+
+def showq_timeframe(showq_df):
+    '''
+    Compute a one-row data frame representing the number of jobs that
+    are in a particular state.
+      showq_df: pandas data frame representing showq information
+      returns a pandas one-row data frame
+    '''
+    time_stamp = get_time_stamp(showq_df)
+    state_distr = state_distribution(showq_df).transpose()
+    state_distr.rename(index={'nr_jobs': time_stamp}, inplace=True)
+    state_distr.index = pd.to_datetime(state_distr.index)
+    return state_distr
+
+def showq_timeseries(directory, pattern='showq_*.csv'):
+    '''
+    Compute a time series for the number of jobs that are in a particular
+    state.
+      directory: Path to the directory containing the showq data files
+      pattern: file name pattern for the showq data files
+      returns pandas data frame representing a time series of the number
+        of jobs in particular states
+    '''
+    showq_files = list(directory.glob(pattern))
+    showq_df = pd.read_csv(showq_files.pop(0))
+    queues_time_series = showq_timeframe(showq_df)
+    for file_name in showq_files:
+        showq_df = pd.read_csv(file_name)
+        new_row = showq_timeframe(showq_df)
+        queues_time_series = pd.concat([queues_time_series, new_row],
+                                       sort=False)
+    queues_time_series.sort_index(inplace=True)
+    return queues_time_series
 
 def moved_jobs(from_cat, to_cat):
     '''
